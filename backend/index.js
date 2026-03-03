826d7edb285bbb3f238159c03fa83db94246c52f
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-04 03:41:07 +0545

    change for deployement

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,15 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 dotenv.config({});
+
+
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
+
+
 
 const app = express();
 app.get("/home", (req, res)=>{
@@ -25,16 +33,17 @@ const corsOptions = {
     origin: "http://localhost:5173", // FIXED
     credentials: true
 };
-
 app.use(cors(corsOptions));
-
-const PORT = process.env.PORT || 3000;
-
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job",  jobRoute);
 app.use("/api/v1/application",applicationRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use((req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 
 
