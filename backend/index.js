579376e93e4a79c14140843be518ad269d3af609
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-04 04:11:08 +0545

    url changes

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173", // FIXED
+    origin:process.env.URL, // FIXED
     credentials: true
 };
 app.use(cors(corsOptions));
