e22f7e5c05db0803621a599b1d877d0699859342
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-04 19:04:11 +0545

    URL changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT ="process.env.URL/api/v1/user";
-export const JOB_API_END_POINT ="process.env.URL/api/v1/job";
-export const APPLICATION_API_END_POINT ="process.env.URL/api/v1/application";
-export const COMPANY_API_END_POINT ="process.env.URL/api/v1/company";
+export const USER_API_END_POINT ="https://job-portal-amar.onrender.com/api/v1/user";
+export const JOB_API_END_POINT ="https://job-portal-amar.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT ="https://job-portal-amar.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT ="https://job-portal-amar.onrender.com/api/v1/company";
+
